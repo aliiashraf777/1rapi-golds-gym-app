5e03b5a5852ea166a919bbabe8408b374360b9ff
Ali Ashraf 777 <aliiashraf777@gmail.com>
2026-04-12 03:21:54 +0500

    feat(navbar): add navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { Stack } from '@mui/material'
 import { Link } from 'react-router-dom'
 import { logo } from '../assets/images'
-
+ 
 const Navbar = () => {
   return (
     <Stack direction='row' justifyContent='space-around' sx={{
